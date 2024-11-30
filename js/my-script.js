ab1313ecc5723a7fc5712a3053756f99c9e99669
pdf26 <patrickfong630@hotmail.com>
2024-11-30 15:52:38 -0800

    Update my-script.js

--- js/my-script.js
+++ js/my-script.js
@@ -30,3 +30,53 @@ let userScore = 0;
 let computerTotal = 0;
 let userTotal = 0;
 let winner = '';
+
+/*
+- dice object
+- array to hold dice values
+- function roll to return a random index number between 0 and 6 
+- this.face to return value at this.values[index]
+*/
+class Dice {
+
+    constructor(){
+
+        this.values = [1,2,3,4,5,6];
+        this.face = 0;
+    }
+}
+
+Dice.prototype.roll = function(){
+    let index = 0; 
+    index = Math.floor(Math.random() * max); 
+   
+    return this.face = this.values[index]; 
+}
+
+/*
+- player object
+- 
+*/
+class Player {
+
+    constructor(name) {
+
+        this.name = name;
+        this.score = 0;
+    }
+}
+
+Player.prototype.getScore = function (dice1, dice2) {
+    this.score;
+
+    if ((dice1 == 1 || dice2 == 1)) {
+        this.score = 0;
+
+    } else if (dice1 == dice2) {
+        this.score = (dice1 + dice2) * 2;
+
+    } else {
+        this.score = (dice1 + dice2);
+    }
+    return this.score;
+}
